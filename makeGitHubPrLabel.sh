dd3c809d3449a9cb47cfecec43f22457e3a4d399
fbeltrao <fbeltrao@windowslive.com>
2019-01-31 18:03:29 +0100

    Release version 0.1.9

--- makeGitHubPrLabel.sh
+++ makeGitHubPrLabel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tsc -p GitHubPRLabel/ && tfx extension create --manifest-globs extensionManifest.json
+tsc -p GitHubPRLabel/ && tfx extension create --manifest-globs GitHubPRLabelManifest.json
